1c397dd961cc1f018c434ab2285c1dde26036486
physer <yuanqiangyuan@orientsoft.cn>
2019-08-08 12:05:21 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 ORACLE_CONF = {
-"URL": "172.24.58.5:1521/orcl",#主机ip地址:端口/实例名,
-"username":"data_pusher",
-"password":"push_szsh",
+"URL": “127.0.0.1:1521/orcl",#主机ip地址:端口/实例名,
+"username":"test",
+"password”:”test”,
 "encoding":"UTF-8",
 "tables":['e_baseinfo','e_li_black','e_inv_person'],
 "comment":"Sensitive",
